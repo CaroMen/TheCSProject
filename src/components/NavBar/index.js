da7ea54f7079335c200de048df647f76a164ca4a
Caro <caroline_mendez@alumni.baylor.edu>
2021-06-12 21:58:09 -0500

    accordion menu started

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,11 +3,44 @@ import { NavLink } from 'react-router-dom';
 
 import './navbar.css';
 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
 const NavBar = () => {
     return (
         <div className="nav-outer">
             <h3 className="logo">The CS Project</h3>
-            <NavLink className="nav-link" to="/bubble-sort">Bubble Sort Visualizer</NavLink>
+            <Accordion allowZeroExpanded>
+                <AccordionItem>
+                    <div className="dropdown-nav">
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="link-header">Sorting Algorithms </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <NavLink className="nav-link" to="/bubble-sort">Bubble Sort Visualizer</NavLink>
+                        </AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <NavLink className="nav-link" to="/insertion-sort">Insertion Sort Visualizer</NavLink>
+                        </AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <NavLink className="nav-link" to="/selection-sort">Selection Sort Visualizer</NavLink>
+                        </AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <NavLink className="nav-link" to="/quick-sort">Quick Sort Visualizer</NavLink>
+                        </AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <NavLink className="nav-link" to="/merge-sort">Merge Sort Visualizer</NavLink>
+                        </AccordionItemPanel>
+                    </div>
+                </AccordionItem>
+            </Accordion>
         </div>
     )
 }
