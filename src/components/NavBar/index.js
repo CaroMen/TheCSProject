440f337e6b89b60f0fd7eb62815f6730150aa2b2
Caro <caroline_mendez@alumni.baylor.edu>
2021-06-13 21:24:06 -0500

    new font

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -27,16 +27,16 @@ const NavBar = () => {
                             <NavLink className="nav-link bubble-sort" to="/bubble-sort">Bubble Sort Visualizer</NavLink>
                         </AccordionItemPanel>
                         <AccordionItemPanel>
-                            <NavLink className="nav-link" to="/insertion-sort">Insertion Sort Visualizer</NavLink>
+                            <NavLink className="nav-link insertion-sort" to="/insertion-sort">Insertion Sort Visualizer</NavLink>
                         </AccordionItemPanel>
                         <AccordionItemPanel>
-                            <NavLink className="nav-link" to="/selection-sort">Selection Sort Visualizer</NavLink>
+                            <NavLink className="nav-link selection-sort" to="/selection-sort">Selection Sort Visualizer</NavLink>
                         </AccordionItemPanel>
                         <AccordionItemPanel>
-                            <NavLink className="nav-link" to="/quick-sort">Quick Sort Visualizer</NavLink>
+                            <NavLink className="nav-link quick-sort" to="/quick-sort">Quick Sort Visualizer</NavLink>
                         </AccordionItemPanel>
                         <AccordionItemPanel>
-                            <NavLink className="nav-link" to="/merge-sort">Merge Sort Visualizer</NavLink>
+                            <NavLink className="nav-link merge-sort" to="/merge-sort">Merge Sort Visualizer</NavLink>
                         </AccordionItemPanel>
                     </div>
                 </AccordionItem>
