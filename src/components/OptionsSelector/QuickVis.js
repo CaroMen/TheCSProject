f7a9cd0fe91a73e1c70ac3185c8cd00e1c5e4251
Caro <caroline_mendez@alumni.baylor.edu>
2021-06-14 21:34:41 -0500

    quick sort info added

--- src/components/OptionsSelector/QuickVis.js
+++ src/components/OptionsSelector/QuickVis.js
@@ -29,6 +29,31 @@ const QuickVis = () => {
 
     return (
         <div className="visualizer-container">
+            <div className="sort-desc">
+                <h3 className="sort-title">Quick Sort</h3>
+                <div className="bubble-desc">This sort algorithm is a divide and conquer algorithm. There will be two empty arrays created intended to hold elements less than the pivot value and greater than the pivot value, which will then be sorted recursively.</div>
+
+                <div className="bubble-desc">There are three important things we need to do to for this algorithm to work.</div>
+
+                <div className="bubble-desc steps">1. Determine the pivot point in the array.</div>
+
+                <div className="bubble-desc steps">2. Create a pointer for the first item in the array.</div>
+
+                <div className="bubble-desc steps">3. Create a pointer for the last item in the array.</div>
+
+                <div className="bubble-desc">We increment our left pointer while our current value is less than the pivot value. We continue this until the left pointer is greater than or equal to the pivot value.</div>
+
+                <div className="bubble-desc">We decrement our right pointer while our current value is greater than the pivot value. We continue this until the right pointer is less than or equal to the pivot value.</div>
+
+                <div className="bubble-desc">Once our pointers have crossde, meaning the left pointer is less than or equal to the right pointer, we swap the values.</div>
+
+                <div className="complexity-desc">
+                    <div className="complexity">Time Complexity:</div>
+                    <div className="complexity-ans">O(n^2)</div>
+                    <div className="space-complexity">Space Complexity:</div>
+                    <div className="complexity-ans">O(1)</div>
+                </div>
+            </div>
             <div className="select-size">
                 <label>Choose array size to display the sorting visualizer:</label>
                 <Slider
